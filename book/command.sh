ad0f59c4c29c25175a8ab95b976b8093829b9112
Christopher Dumas <christopherdumas@gmail.com>
2021-12-10 13:35:42 -0800

    fix a few more spelling errors

--- book/command.sh
+++ book/command.sh
@@ -1,1 +1,2 @@
 pandoc --metadata-file=metadata.yml -V geometry:papersize="{5.5in,8.5in}" -V geometry:margin="0.5in" -V geometry:top="1in" -V geometry:bottom="1in" --standalone --template ct.tex chapter*.md -o book.pdf
+pandoc --metadata-file=metadata_epub.yml chapter*.md -o book.epub
